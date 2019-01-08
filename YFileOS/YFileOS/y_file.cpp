15a76163ef1de6c0845214219541135c7d9cc4bd
yangqinshun <yangqinshun@h3d.com.cn>
2019-01-08 15:14:03 +0800

    实现 YFile 接口

--- YFileOS/YFileOS/y_file.cpp
+++ YFileOS/YFileOS/y_file.cpp
@@ -1,20 +1,117 @@
 #include "y_file.h"
 
 
-YIFile::YIFile()
-{
-}
-
-YIFile::~YIFile()
-{
-}
 
 //------YFile----------------------------------------------------------------//
-YFile::YFile()
-	:YIFile()
+YFile::YFile(YFileType rFileType)
+	: YIFile()
+	, m_eFileType(rFileType)
+	, m_nFileDataSize(0)
+	, m_szModifyDate("")
 {
 }
 
 YFile::~YFile()
 {
+}
+
+const std::int8_t * YFile::getFileData()
+{
+	if (m_pData.empty())
+		return nullptr;
+	return &m_pData[0];
+}
+
+const std::uint32_t YFile::getFileSize()
+{
+	return m_nFileDataSize;
+}
+
+const std::string & YFile::getModifyDate()
+{
+	return m_szModifyDate
+}
+
+const std::uint32_t YFile::getChildrenCount()
+{
+	return m_rChildrenArr.size();
+}
+
+std::vector<YFile*>& YFile::getChildren()
+{
+	return m_rChildrenArr;
+}
+
+const std::string & YFile::getName()
+{
+	return m_szName;
+}
+
+void YFile::setFileData(int8_t * data, uint32_t size)
+{
+	if (nullptr == data || 0 <= size)
+		return;
+	m_pData.resize(size);
+	memcpy(&m_pData[0], data, size);
+}
+
+void YFile::setModifyDate(const std::string & modifyDate)
+{
+	m_szModifyDate = modifyDate;
+}
+
+void YFile::addChild(YFile * child)
+{
+	if (nullptr == child)
+		return;
+	m_rChildrenArr.push_back(child);
+}
+
+void YFile::delChild(YFile * child)
+{
+	if (nullptr == child)
+		return;
+	for (auto rIter = m_rChildrenArr.begin(); rIter != m_rChildrenArr.end();)
+	{
+		if (child == *rIter)
+		{
+			m_rChildrenArr.erase(rIter);
+		}
+		else
+		{ 
+			++rIter;
+		}
+	}
+}
+
+void YFile::setName(const std::string & name)
+{
+	if (name.empty())
+		return;
+	m_szName = name;
+}
+
+bool YFile::IsFile()
+{
+	return Y_File == m_eFileType;
+}
+
+bool YFile::IsFolder()
+{
+	return Y_Folder == m_eFileType;
+}
+
+bool YFile::IsRealSymLnk()
+{
+	return Y_SymLnk == m_eFileType;
+} 
+
+bool YFile::IsRealFolder()
+{
+	return Y_Folder == m_eFileType;
+}
+
+bool YFile::IsRealFile()
+{
+	return Y_File == m_eFileType;
 }
--- YFileOS/YFileOS/y_file.h
+++ YFileOS/YFileOS/y_file.h
@@ -1,40 +1,51 @@
 #pragma once
-#include <vector>
-
+#include "y_ifile.h"
 using namespace std;
-class YIFile
+
+enum YFileType
 {
-public:
-	virtual ~YIFile();
-	YIFile();
-
-	virtual const int8_t* getFileData() = 0;
-	virtual const uint32_t getFileSize() = 0;
-	virtual const string  getModifyDate() = 0;
-	virtual bool IsFile() = 0;
-	virtual bool IsFolder() = 0;
-	virtual bool IsSymLnk() = 0;
-private:
-
+	Y_File,
+	Y_Folder,
+	Y_SymLnk
 };
-
 class YFile : public YIFile
 {
-	enum YFileType
-	{
-		Y_File,
-		Y_Folder,
-		Y_SymLnk
-	};
+
 public:
-	YFile();
+	YFile(YFileType rFileType = Y_File );
 	virtual ~YFile();
+	//读接口
+	virtual const std::int8_t*		getFileData();
+	virtual const std::uint32_t		getFileSize();
+	virtual const std::string&		getModifyDate();
+	virtual const std::uint32_t		getChildrenCount();
+	virtual std::vector<YFile*>&	getChildren();
+	virtual const std::string&		getName();
+
+	//写接口
+	void	setFileData(int8_t* data,uint32_t size);
+	void	setModifyDate(const std::string& modifyDate);
+	void	addChild(YFile* child);
+	void	delChild(YFile* child);
+	void	setName(const std::string& name);
+
+
+	//验证文件类型..Real为实际类型，Isxxx接口有可能是符号链接文件指向一个文件夹
+	virtual bool IsFile();
+	virtual bool IsFolder();
+	virtual bool IsRealSymLnk();
+	virtual bool IsRealFolder();
+	virtual bool IsRealFile();
+
+
 
 private:
-	vector<int8_t*>	m_pData;
-	string			m_szName;
-	string			m_szModifyDate;
-	const YFileType	m_eFileType;
+	std::vector<int8_t>			m_pData;
+	std::string					m_szName;
+	std::string					m_szModifyDate;
+	const YFileType				m_eFileType;
+	std::uint32_t				m_nFileDataSize;
 
+	std::vector<YFile*>		    m_rChildrenArr;
 };
 
